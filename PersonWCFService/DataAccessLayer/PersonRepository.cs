5cb69acb7dc5ad8d6d62387049d71766b27c41f7
Felix Miranda <felixmiranda.net@gmail.com>
2025-01-29 17:35:05 -0500

    Add project files.

---
+++ PersonWCFService/DataAccessLayer/PersonRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class PersonRepository
+{
+    // Simulamos una base de datos con una lista en memoria
+    private readonly List<PersonResponse> _persons = new List<PersonResponse>
+        {
+            new PersonResponse
+            {
+                CellPhoneNumber = "123456789",
+                Name = "John",
+                LastName = "Doe",
+                DocumentType = "ID",
+                DocumentNumber = "12345678"
+            },
+            new PersonResponse
+            {
+                CellPhoneNumber = "987654321",
+                Name = "Jane",
+                LastName = "Smith",
+                DocumentType = "Passport",
+                DocumentNumber = "87654321"
+            }
+        };
+
+    public PersonResponse GetPersonByCellPhoneNumber(string cellPhoneNumber)
+    {
+        return _persons.FirstOrDefault(p => p.CellPhoneNumber == cellPhoneNumber);
+    }
+}
